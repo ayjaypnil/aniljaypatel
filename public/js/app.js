b9afb667cb2db1f67832ffc69ede899edece2031
Anil Patel <ajpatel92@icloud.com>
2018-03-31 15:55:50 -0500

    main page structure complete

---
+++ public/js/app.js
@@ -0,0 +1,11 @@
+/* Set the width of the side navigation to 250px */
+$("#openNav").on("click", function() {
+  document.getElementById("mySidenav").style.width = "50%";
+});
+
+/* Set the width of the side navigation to 0 */
+$("#closeBtn").on("click", function() {
+
+ document.getElementById("mySidenav").style.width = "0";
+});
+
---
+++ server.js
@@ -0,0 +1,25 @@
+// Require Express for routing
+var express = require("express");
+var app = express();
+// Defining local port or assigned port when deployed
+var PORT = process.env.PORT || 3000;
+
+// Require EJS for templating
+var ejs = require("ejs");
+// Templating engine will look to views folder
+app.set("view engine", "ejs");
+// Serving up everything in the public folder 
+app.use(express.static("public"));
+
+
+
+// Route to home page
+app.get("/", function(req, res) {
+    res.render(__dirname + "/views/main.ejs");
+}); 
+
+// App is listening on local 3000 or assigned port
+app.listen(PORT, function(err) {
+  if (err) throw err;
+  console.log("Personal Portfolio Initializing...  (" + PORT + ")");
+});
