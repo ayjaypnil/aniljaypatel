0eaf7887a1e99bd82d9e10170ffbd303dc31bc07
Anil Patel <ajpatel92@icloud.com>
2018-09-07 14:19:22 -0500

    edits

--- server.js
+++ server.js
@@ -8,30 +8,33 @@ var PORT = process.env.PORT || 3000;
 var ejs = require("ejs");
 // Templating engine will look to views folder
 app.set("view engine", "ejs");
-// Serving up everything in the public folder 
+// Serving up everything in the public folder
 app.use(express.static("public"));
-
-
 
 // Route to home page
 app.get("/", function(req, res) {
-    res.render(__dirname + "/views/main.ejs");
-}); 
+  res.render(__dirname + "/views/main.ejs");
+});
 
 // Route to project page
 app.get("/project", function(req, res) {
-    res.render(__dirname + "/views/project.ejs");
-}); 
+  res.render(__dirname + "/views/project.ejs");
+});
 
-// Route to project page
+// Route to law/resume page
 app.get("/law", function(req, res) {
-    res.render(__dirname + "/views/law.ejs");
-}); 
+  res.render(__dirname + "/views/law.ejs");
+});
 
-// Route to project page
+// Route to blog page
 app.get("/blog", function(req, res) {
-    res.render(__dirname + "/views/blog.ejs");
-}); 
+  res.render(__dirname + "/views/blog.ejs");
+});
+
+// Route to contact page
+app.get("/contact", function(req, res) {
+  res.render(__dirname + "/views/contact.ejs");
+});
 
 // App is listening on local 3000 or assigned port
 app.listen(PORT, function(err) {
